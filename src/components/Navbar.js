7c50d9b4a9d16389b64561a689b8294a14cc8f36
Lyndsie Lane <lyndsielane@yahoo.com>
2021-08-24 13:24:08 -0400

    working oon nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import '../styles/Navbar.css';
-
-
-const styles = {
-  navbarStyle: {
-    background: 'green',
-    justifyContent: 'flex-end',
-  },
-};
+import React from "react";
+import "../styles/Navbar.css";
 
 function Navbar() {
   return (
-    <nav style={styles.navbarStyle} className="navbar">
-      <a href="/">Welcome</a>
+    <nav>
+      <div class="nav-bg"></div>
+      <img
+        src="./assets/Photos/LL Circle Template.png"
+        alt="lyndsie-logo"
+        class="logo"
+      />
+      <button class="hamburger" id="hamburger">
+        <i class="fas fa-bars"></i>
+      </button>
+      <ul class="nav-ul" id="nav-ul">
+        <li>
+          <a href="#about" alt="about-me">
+            About Me
+          </a>
+        </li>
+        <li>
+          <a href="#my-work" alt="my-work">
+            My Work
+          </a>
+        </li>
+        <li>
+          <a href="#contact-me" alt="contact-me">
+            Contact Me
+          </a>
+        </li>
+        <li>
+          <a
+            href="./assets/Files/Lyndsie Lane Resume.pdf"
+            alt="resume"
+            target="blank"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
